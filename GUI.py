1b5a60919b6cde3a02f987630b049e35baf9259a
iael <jcs72484@udelasalle.edu.mx>
2022-12-01 16:33:25 -0600

    Final

--- GUI.py
+++ GUI.py
@@ -2,6 +2,8 @@ import os
 from tkinter import *
 import tkinter.messagebox
 from tkinter import filedialog
+import PySimpleGUI as sg
+from datetime import date
 
 import customtkinter
 import cv2
@@ -29,9 +31,6 @@ class App(customtkinter.CTk):
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
 
-        # ============ create two frames ============
-
-        # configure grid layout (2x1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
@@ -43,13 +42,10 @@ class App(customtkinter.CTk):
         self.frame_right = customtkinter.CTkFrame(master=self)
         self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
 
-        # ============ frame_left ============
-
-        # configure grid layout (1x11)
-        self.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
-        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
-        self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
-        self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(0, minsize=10)
+        self.frame_left.grid_rowconfigure(5, weight=1)
+        self.frame_left.grid_rowconfigure(8, minsize=20)
+        self.frame_left.grid_rowconfigure(11, minsize=10)
 
         self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
                                               text="Opciones",
@@ -74,9 +70,6 @@ class App(customtkinter.CTk):
                                                         command=self.change_appearance_mode)
         self.optionmenu_1.grid(row=10, column=0, pady=10, padx=20, sticky="w")
 
-        # ============ frame_right ============
-
-        # configure grid layout (3x7)
         self.label_2 = customtkinter.CTkLabel(master=self.frame_right,
                                               text="Imagen Original",
                                               text_font=("Roboto Medium", -16))  # font name and size in px
@@ -113,7 +106,44 @@ class App(customtkinter.CTk):
         self.optionmenu_1.set("Dark")
 
     def button_event(self):
-        print("Button pressed")
+
+        camara = cv2.VideoCapture(0)
+
+        sg.theme('DarkBlue')
+
+        layout = [[sg.Image(filename='', key='-image-')],
+                  [sg.Button('Tomar Fotografia'), sg.Button('Salir')]]
+
+        window = sg.Window('Camara FACIALIX',
+                           layout,
+                           no_titlebar=False,
+                           location=(0, 0))
+
+        image_elem = window['-image-']
+
+        numero = 0
+
+        while camara.isOpened():
+
+            event, values = window.read(timeout=0)
+            ret, frame = camara.read()
+
+            if event in ('Exit', None):
+                window.close()
+                camara.release()
+                break
+
+            elif event == 'Tomar Fotografia':
+                ruta = sg.popup_get_folder(title='Guardar Fotografia', message="Carpeta destino")
+                cv2.imwrite(ruta + "/" + str(date.today()) + str(numero) + ".png", frame)
+
+            if not ret:
+
+                break
+
+            imgbytes = cv2.imencode('.png', frame)[1].tobytes()  # ditto
+            image_elem.update(data=imgbytes)
+            numero = numero + 1
 
     def change_appearance_mode(self, new_appearance_mode):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -123,17 +153,29 @@ class App(customtkinter.CTk):
         image = cv2.imread(path)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (11, 11), 0)
-        canny = cv2.Canny(blur, 35, 70)
+        canny = cv2.Canny(blur, 0, 115)
         dilated = cv2.dilate(canny, (1, 1), iterations=0)
         cnt, hierarchy = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         rgb2 = cv2.drawContours(rgb, cnt, -1, (0, 255, 0), 2)
 
         image = Image.fromarray(image)
+        image = image.resize((700, 500))
         image = ImageTk.PhotoImage(image)
 
+        TKINTER_WIDGETS['label_img'] = customtkinter.CTkLabel(master=self.frame_right, image=image)
+        TKINTER_WIDGETS['label_img'].image = image
+        TKINTER_WIDGETS['label_img'].grid(row=1, column=0, padx=10, pady=10)
+
         rgb2 = Image.fromarray(rgb2)
+        rgb2 = rgb2.resize((700, 500))
         rgb2 = ImageTk.PhotoImage(rgb2)
+
+        TKINTER_WIDGETS['label2_img'] = customtkinter.CTkLabel(master=self.frame_right, image=rgb2)
+        TKINTER_WIDGETS['label2_img'].image = rgb2
+        TKINTER_WIDGETS['label2_img'].grid(row=1, column=1, padx=10, pady=10)
+
+        self.label_4.configure(text=f"Numero de Objetos: {len(cnt)}")
 
     def on_closing(self, event=0):
         self.destroy()
